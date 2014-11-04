4c4b213674cf79c6b2edcb9546e5af7f79121184
Hanyu Zhang <hyzhangsf@gmail.com>
2014-11-04 04:16:11 -0800

    q1 done

--- inference.py
+++ inference.py
@@ -150,17 +150,20 @@ class ExactInference(InferenceModule):
         pacmanPosition = gameState.getPacmanPosition()
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # util.raiseNotDefined()
 
         # Replace this code with a correct observation update
         # Be sure to handle the "jail" edge case where the ghost is eaten
         # and noisyDistance is None
         allPossible = util.Counter()
-        for p in self.legalPositions:
-            trueDistance = util.manhattanDistance(p, pacmanPosition)
-            if emissionModel[trueDistance] > 0:
-                allPossible[p] = 1.0
-
+        if noisyDistance is None:
+            #  ghost captured
+            allPossible[self.getJailPosition()] = 1
+        else:
+            for p in self.legalPositions:
+                trueDistance = util.manhattanDistance(p, pacmanPosition)
+                if emissionModel[trueDistance] > 0:
+                    allPossible[p] = self.beliefs[p]*emissionModel[trueDistance]
         "*** END YOUR CODE HERE ***"
 
         allPossible.normalize()
